d4c6fc0fb544baa3873f7e3feafeee56c03d0b09
JBENITO <JBENITO@UBVPNN6752.core.unionbank.com>
2026-01-07 16:11:36 +0800

    Add CRUD logic and utility functions for comments

--- src/index.js
+++ src/index.js
@@ -5,9 +5,34 @@ import {
     getUserById,
     addUser,
 } from "./services/users";
+import {
+    getCommentById,
+    getCommentsByPostId,
+    updateCommentBody,
+    deleteCommentById,
+    addComment,
+} from "./services/comments.js";
 
-updateUser(2, { email: "jaynard@soft.en" });
+updateUser(3, { email: "jaynard@soft.en" });
 
 console.log(addUser());
-console.log(getUserById(2));
+console.log(getUserById(3));
 console.log(getAllUsers());
+
+const newEntry = addComment({
+    postId: 1,
+    body: "This is a new comment!",
+    author: "DevUser",
+});
+console.log("Added:", newEntry);
+
+const postComments = getCommentsByPostId(1);
+console.log("Comments for Post 1:", postComments);
+
+const updated = updateCommentBody(newEntry.id, "This body has been updated!");
+console.log("Updated Comment:", updated);
+
+deleteCommentById(newEntry.id);
+console.log("all Comments Deleted");
+
+console.log(getCommentById(3));
--- src/services/comments.js
+++ src/services/comments.js
@@ -1,25 +1,41 @@
 import { comments as data } from "../data/comments";
+import { generateId } from "../utils";
 
 //private
 let comments = [...data];
 
-function getCommentById(id) {
-  //get comment by id
+export function getCommentById(id) {
+    //get comment by id
+    return comments.find((comment) => comment.id === id);
 }
 
-function getCommentsByPostId(postId) {
-  //get comments by post id
+export function getCommentsByPostId(postId) {
+    //get comments by post id
+    return comments.filter((comment) => comment.postId === postId);
 }
 
-function updateCommentBody(id, body) {
-  //update comment body
+export function updateCommentBody(id, body) {
+    //update comment body
+    comments = comments.map((comment) =>
+        comment.id === id ? { ...comment, body: body } : comment
+    );
+    return getCommentById(id);
 }
 
-function deleteCommentById(id) {
-  //delete comment by id
+export function deleteCommentById(id) {
+    //delete comment by id
+    comments = comments.filter((comment) => comment.id !== id);
+    return { message: `Comment ${id} deleted successfully` };
 }
 
-function addComment(comment) {
-  //add comment to comments array
-  // use generateId function and pass comments array as the argument to generate a unique id.
+export function addComment(comment) {
+    //add comment to comments array
+    // use generateId function and pass comments array as the argument to generate a unique id.
+    const newComment = {
+        ...comment,
+        // Use the function from util.js
+        id: generateId(comments),
+    };
+    comments.push(newComment);
+    return newComment;
 }
